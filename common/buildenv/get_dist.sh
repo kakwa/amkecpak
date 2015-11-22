9357dfd44770bf40a072ca4774001ea63ea63911
kakwa <carpentier.pf@gmail.com>
2015-11-22 14:11:52 +0100

    fix get_dist script

--- common/buildenv/get_dist.sh
+++ common/buildenv/get_dist.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 rhr="/etc/redhat-release"
-if ! [ -f "" ]
+if ! [ -f "$rhr" ]
 then
     printf 'unk\n'
     exit 0
 fi
 
 gen_dist(){
-    pref=""
-    count=""
-    cn=""
-    if grep -q ""  ""
+    pref="$1"
+    count="$2"
+    cn="$3"
+    if grep -q "$cn"  "$rhr"
     then
-        printf "\n"
+        printf "${pref}${count}\n"
         exit 0
     fi
 }
@@ -21,17 +21,17 @@ gen_dist(){
 fc_releases="Yarrow Tettnang Heidelberg Stentz Bordeaux Zod Moonshine Werewolf Sulphur Cambridge Leonidas Constantine Goddard Laughlin Lovelock Verne Miracle Spherical Schrödinger Heisenbug"
 
 c=1
-for i in 
+for i in $fc_releases
 do
-    gen_dist "fc" "" ""
-    c=1
+    gen_dist "fc" "$c" "$i"
+    c=$(( $c + 1 ))
 done
 
 rhel_releases="NONE Pensacola Taroon Nahant Tikanga Santiago Maipo"
 
 c=1
-for i in 
+for i in $rhel_releases
 do
-    gen_dist "el" "" ""
-    c=1
+    gen_dist "el" "$c" "$i"
+    c=$(( $c + 1 ))
 done
