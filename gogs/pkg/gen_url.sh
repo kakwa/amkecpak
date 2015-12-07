33cbf1ad8839f20e7580d7d817d70a0922d4863a
kakwa <carpentier.pf@gmail.com>
2015-12-08 00:42:36 +0100

    update gogs + rework of deps handling

--- gogs/pkg/gen_url.sh
+++ gogs/pkg/gen_url.sh
@@ -6,53 +6,61 @@ cat Makefile | while read line
 do
     if echo $line | grep -qe '^VERSION_'
     then
-        name=`echo $line | sed "s/VERSION_//" | sed "s/_/\//" | sed "s/_/\//" | sed "s/=.*//"`
+        name=`echo $line | sed "s/VERSION_//" | sed "s/=.*//"`
         name_under=`echo $line | sed "s/VERSION_//" | sed "s/=.*//"`
         var_version=`echo $line | sed "s/=.*//"`
         var_url=`echo $line | sed "s/VERSION_/URL_/" | sed "s/=.*//"`
         var_path=`echo $line | sed "s/VERSION_/PATH_/" | sed "s/=.*//"`
         name_url=$name
+        file=`echo $name |sed 's/\//-/g'`
 
-        if echo $name_url | grep -qe '^x/'
+        if echo $name_url | grep -qe '^golang.org/x'
         then
-            name_url=`echo $name | sed 's|^x/|golang/|'`
-            name="golang.org/$name"
+            name_url=`echo $name | sed 's|^golang.org/x|github.com/golang|'`
+
         elif echo $name_url | grep -qe 'redis.v2'
         then
             name_url=`echo $name | sed 's|redis.v2|go-redis/redis|'`
-            name="gopkg.in/$name"
-        elif echo $name_url | grep -qe 'gomail.v2'
-        then
-            name_url=`echo $name | sed 's|gomail.v2|go-gomail/gomail|'`
-            name="gopkg.in/$name"
+
         elif echo $name_url | grep -qe 'ini.v1'
         then
             name_url=`echo $name | sed 's|ini.v1|go-ini/ini|'`
-            name="gopkg.in/$name"
+
         elif echo $name_url | grep -qe 'macaron.v1'
         then
             name_url=`echo $name | sed 's|macaron.v1|go-macaron/macaron|'`
-            name="gopkg.in/$name"
+
         elif echo $name_url | grep -qe 'bufio.v1'
         then
             name_url=`echo $name | sed 's|bufio.v1|go-bufio/bufio|'`
-            name="gopkg.in/$name"
+
         elif echo $name_url | grep -qe 'alexcesaro/quotedprintable.v3'
         then
             name_url=`echo $name | sed 's|alexcesaro/quotedprintable.v3|alexcesaro/quotedprintable|'`
-            name="gopkg.in/$name"
-        elif echo $name_url | grep -qe 'anchor_name'
+
+        elif echo $name_url | grep -qe 'gomail.v2'
         then
-            name=`echo $name | sed 's|sanitized/anchor_name|sanitized_anchor_name|'`
-            name_url=$name
-            name="github.com/$name"
-        else
-            name="github.com/$name"
+            name_url=`echo $name | sed 's|gomail.v2|go-gomail/gomail|'`
+
+        elif echo $name_url | grep -qe 'ldap.v2'
+        then
+            name_url=`echo $name | sed 's|ldap.v2|go-ldap/ldap|'`
+
+        elif echo $name_url | grep -qe 'asn1-ber.v1'
+        then
+            name_url=`echo $name | sed 's|asn1-ber.v1|go-asn1-ber/asn1-ber|'`
+
         fi
-        echo "$var_url=https://github.com/$name_url/archive/\$($var_version).tar.gz" >> Makefile.meta
+
+        if echo $name_url | grep -qe 'gopkg.in'
+        then
+            name_url=`echo $name_url | sed 's|gopkg.in|github.com|'`
+        fi
+
+        echo "$var_url=https://$name_url/archive/\$($var_version).tar.gz" >> Makefile.meta
         echo "$var_path=$name" >> Makefile.meta
-        printf "\t\$(WGS) -u \$($var_url) -o \$(BUILD_DIR)/$name_under-\$(${var_version}).tar.gz\n" >> Makefile.get
-        printf "\t\$(GO_EXT) -a \$(BUILD_DIR)/$name_under-\$(${var_version}).tar.gz -n \$($var_path)\n" >>Makefile.get
-        printf "\trm \$(BUILD_DIR)/$name_under-\$(${var_version}).tar.gz\n" >> Makefile.get
+        printf "\t\$(WGS) -u \$($var_url) -o \$(BUILD_DIR)/$file-\$(${var_version}).tar.gz\n" >> Makefile.get
+        printf "\t\$(GO_EXT) -a \$(BUILD_DIR)/$file-\$(${var_version}).tar.gz -n \$($var_path)\n" >>Makefile.get
+        printf "\trm \$(BUILD_DIR)/$file-\$(${var_version}).tar.gz\n" >> Makefile.get
     fi
 done
