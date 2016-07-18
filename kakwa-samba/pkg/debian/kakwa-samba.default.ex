d6c1e68ae7add37f1ffca85e1a3fcb76d95dd4db
kakwa <carpentier.pf@gmail.com>
2016-07-18 23:48:01 +0200

    rough packaging for samba

---
+++ kakwa-samba/pkg/debian/kakwa-samba.default.ex
@@ -0,0 +1,10 @@
+# Defaults for dash initscript
+# sourced by /etc/init.d/dash
+# installed at /etc/default/dash by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
