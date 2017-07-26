e3e72294ebdce5099bc00ef2ee280d37c2d258ef
kakwa <carpentier.pf@gmail.com>
2017-07-26 02:34:59 +0200

    adding better handling for many distro version

--- common/buildenv/get_dist.sh
+++ common/buildenv/get_dist.sh
@@ -2,19 +2,58 @@
 
 
 case $1 in
+  # Debian code names
   wheezy)
-    echo 7;;
+    echo 7:deb;;
   jessie)
-    echo 8;;
+    echo 8:deb;;
   stretch)
-    echo 9;;
+    echo 9:deb;;
   buster)
-    echo 10;;
+    echo 10:deb;;
   sid)
-    echo 10;;
+    echo 10:deb;;
+  # Ubuntu code names
+  precise)
+    echo 12.4:ubu;;
+  trusty)
+    echo 14.4:ubu;;
+  vivid)
+    echo 15.4:ubu;;
+  wily)
+    echo 15.10:ubu;;
+  xenial)
+    echo 16.4:ubu;;
+  yakkety)
+    echo 16.10:ubu;;
+  zesty)
+    echo 17.4:ubu;;
+  artful)
+    echo 17.10:ubu;;
+  # Fedora code names
+  fc24)
+    echo 24:fc;;
+  fc25)
+    echo 25:fc;;
+  fc26)
+    echo 26:fc;;
+  fc27)
+    echo 27:fc;;
+  fc28)
+    echo 28:fc;;
+  fc29)
+    echo 29:fc;;
+  fc30)
+    echo 30:fc;;
+  # RHEL code names
+  el6)
+    echo 6:el;;
+  el7)
+    echo 7:el;;
+  # default
   unknown)
-    echo 00;;
+    echo 0:unk;;
   *)
-    echo $1;;
+    echo 0:unk;;
 esac
 
