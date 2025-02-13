7d01dc4e0fc2a7fc0003a3f00e23fd53e88b2f6d
kakwa <carpentier.pf@gmail.com>
2025-02-13 21:50:56 +0100

    clean-up script

--- common/buildenv/get_dist.sh
+++ common/buildenv/get_dist.sh
@@ -1,129 +1,72 @@
 #!/bin/sh
-#
+
+
+help() {
+  cat <<EOF
+Usage: $(basename "$0") <distro_version>
+
+Maps a distribution name/version to a standardized format.
+
+Supported Distributions:
+  - Debian:      deb<N> (e.g., deb10) or codename (e.g., buster)
+  - Ubuntu:      ubu<N>.<M> (e.g., ubu20.04) or codename (e.g., focal)
+  - Fedora:      fc<N> (e.g., fc40)
+  - RHEL/CentOS: el<N> (e.g., el9)
+
+Examples:
+  $(basename "$0") focal
+  $(basename "$0") ubu22.04
+  $(basename "$0") fc39
+  $(basename "$0") el8
+EOF
+  exit 1
+}
+
+# Validate input arguments
+[ "$#" -ne 1 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]  && help
+
 case $1 in
-  # Debian code names and version mapping:
-  wheezy|deb7)
-    echo 7:deb:debian-7; exit 0;;
-  jessie|deb8)
-    echo 8:deb:debian-8; exit 0;;
-  stretch|deb9)
-    echo 9:deb:debian-9; exit 0;;
-  buster|deb10)
-    echo 10:deb:debian-10; exit 0;;
-  bullseye|deb11)
-    echo 11:deb:debian-11; exit 0;;
-  bookworm|deb12)
-    echo 12:deb:debian-12; exit 0;;
-  trixie|deb13)
-    echo 13:deb:debian-13; exit 0;;
-  sid|deb14)
-    echo 14:deb:debian-14; exit 0;;
+  # Debian versions
+  wheezy|deb7) echo "7:deb:debian-7" ;;
+  jessie|deb8) echo "8:deb:debian-8" ;;
+  stretch|deb9) echo "9:deb:debian-9" ;;
+  buster|deb10) echo "10:deb:debian-10" ;;
+  bullseye|deb11) echo "11:deb:debian-11" ;;
+  bookworm|deb12) echo "12:deb:debian-12" ;;
+  trixie|deb13) echo "13:deb:debian-13" ;;
+  sid|deb14) echo "14:deb:debian-14" ;;
 
-  # Ubuntu code names and version mapping:
-  precise|ubu12.4)
-    echo 12.4:ubu:ubuntu-12.4; exit 0;;
-  trusty|ubu14.4)
-    echo 14.4:ubu:ubuntu-14.4; exit 0;;
-  vivid|ubu15.4)
-    echo 15.4:ubu:ubuntu-15.4; exit 0;;
-  wily|ubu15.10)
-    echo 15.10:ubu:ubuntu-15.10; exit 0;;
-  xenial|ubu16.4)
-    echo 16.4:ubu:ubuntu-16.4; exit 0;;
-  yakkety|ubu16.10)
-    echo 16.10:ubu:ubuntu-16.10; exit 0;;
-  zesty|ubu17.4)
-    echo 17.4:ubu:ubuntu-17.4; exit 0;;
-  artful|ubu17.10)
-    echo 17.10:ubu:ubuntu-17.10; exit 0;;
-  bionic|ubu18.4)
-    echo 18.4:ubu:ubuntu-18.4; exit 0;;
-  cosmic|ubu18.10)
-    echo 18.10:ubu:ubuntu-18.10; exit 0;;
-  disco|ubu19.4)
-    echo 19.4:ubu:ubuntu-19.4; exit 0;;
-  eoan|ubu19.10)
-    echo 19.10:ubu:ubuntu-19.10; exit 0;;
-  focal|ubu20.4)
-    echo 20.4:ubu:ubuntu-20.4; exit 0;;
-  groovy|ubu20.10)
-    echo 20.10:ubu:ubuntu-20.10; exit 0;;
-  hirsute|ubu21.04)
-    echo 21.04:ubu:ubuntu-21.04; exit 0;;
-  impish|ubu21.10)
-    echo 21.10:ubu:ubuntu-21.10; exit 0;;
-  jammy|ubu22.04)
-    echo 22.04:ubu:ubuntu-22.04; exit 0;;
-  lunar|ubu23.04)
-    echo 23.04:ubu:ubuntu-23.04; exit 0;;
-  mantic|ubu23.10)
-    echo 23.10:ubu:ubuntu-23.10; exit 0;;
-  noble|ubu24.04)
-    echo 24.04:ubu:ubuntu-24.04; exit 0;;
-  oracular|ubu24.10)
-    echo 24.10:ubu:ubuntu-24.10; exit 0;;
-  plucky|ubu25.04)
-    echo 25.04:ubu:ubuntu-25.04; exit 0;;
+  # Ubuntu versions
+  precise|ubu12.04) echo "12.04:ubu:ubuntu-12.04" ;;
+  trusty|ubu14.04) echo "14.04:ubu:ubuntu-14.04" ;;
+  vivid|ubu15.04) echo "15.04:ubu:ubuntu-15.04" ;;
+  wily|ubu15.10) echo "15.10:ubu:ubuntu-15.10" ;;
+  xenial|ubu16.04) echo "16.04:ubu:ubuntu-16.04" ;;
+  yakkety|ubu16.10) echo "16.10:ubu:ubuntu-16.10" ;;
+  zesty|ubu17.04) echo "17.04:ubu:ubuntu-17.04" ;;
+  artful|ubu17.10) echo "17.10:ubu:ubuntu-17.10" ;;
+  bionic|ubu18.04) echo "18.04:ubu:ubuntu-18.04" ;;
+  cosmic|ubu18.10) echo "18.10:ubu:ubuntu-18.10" ;;
+  disco|ubu19.04) echo "19.04:ubu:ubuntu-19.04" ;;
+  eoan|ubu19.10) echo "19.10:ubu:ubuntu-19.10" ;;
+  focal|ubu20.04) echo "20.04:ubu:ubuntu-20.04" ;;
+  groovy|ubu20.10) echo "20.10:ubu:ubuntu-20.10" ;;
+  hirsute|ubu21.04) echo "21.04:ubu:ubuntu-21.04" ;;
+  impish|ubu21.10) echo "21.10:ubu:ubuntu-21.10" ;;
+  jammy|ubu22.04) echo "22.04:ubu:ubuntu-22.04" ;;
+  lunar|ubu23.04) echo "23.04:ubu:ubuntu-23.04" ;;
+  mantic|ubu23.10) echo "23.10:ubu:ubuntu-23.10" ;;
+  noble|ubu24.04) echo "24.04:ubu:ubuntu-24.04" ;;
+  oracular|ubu24.10) echo "24.10:ubu:ubuntu-24.10" ;;
+  plucky|ubu25.04) echo "25.04:ubu:ubuntu-25.04" ;;
 
-  # Fedora code names:
-  fc24)
-    echo 24:fc:fedora-24; exit 0;;
-  fc25)
-    echo 25:fc:fedora-25; exit 0;;
-  fc26)
-    echo 26:fc:fedora-26; exit 0;;
-  fc27)
-    echo 27:fc:fedora-27; exit 0;;
-  fc28)
-    echo 28:fc:fedora-28; exit 0;;
-  fc29)
-    echo 29:fc:fedora-29; exit 0;;
-  fc30)
-    echo 30:fc:fedora-30; exit 0;;
-  fc31)
-    echo 31:fc:fedora-31; exit 0;;
-  fc32)
-    echo 32:fc:fedora-32; exit 0;;
-  fc33)
-    echo 33:fc:fedora-33; exit 0;;
-  fc34)
-    echo 34:fc:fedora-34; exit 0;;
-  fc35)
-    echo 35:fc:fedora-35; exit 0;;
-  fc36)
-    echo 36:fc:fedora-36; exit 0;;
-  fc37)
-    echo 37:fc:fedora-37; exit 0;;
-  fc38)
-    echo 38:fc:fedora-38; exit 0;;
-  fc39)
-    echo 39:fc:fedora-39; exit 0;;
-  fc40)
-    echo 40:fc:fedora-40; exit 0;;
- 
-  # RHEL/CentOS code names:
-  el6)
-    echo 6:el:rocky+epel-6; exit 0;;
-  el7)
-    echo 7:el:rocky+epel-7; exit 0;;
-  el8)
-    echo 8:el:rocky+epel-8; exit 0;;
-  el9)
-    echo 9:el:rocky+epel-9; exit 0;;
-  el10)
-    echo 10:el:rocky+epel-10; exit 0;;
-  el11)
-    echo 11:el:rocky+epel-11; exit 0;;
+  # Generic Mapping for fc/RHEL
+  fc[0-9]*) echo "${1#fc}:fc:fedora-${1#fc}" ;;
+  el[0-9]*) echo "${1#el}:el:rocky+epel-${1#el}" ;;
+  # Generic Debian/Ubuntu mapping
+  deb[0-9]*) echo "${1#deb}:deb:debian-${1#deb}" ;;
+  ubu[0-9]*\.[0-9]*) echo "${1#ubu}:ubu:ubuntu-${1#ubu}" ;;
 
-  # Generic mapping case:
-  deb[0-9]*)
-    ver=`echo $1 | sed 's/deb//'`; echo "$ver:deb:debian-$ver"; exit 0;;
-  ubu[0-9]*\.[0-9]*)
-    ver=`echo $1 | sed 's/ubu//'`; echo "$ver:ubu:ubuntu-$ver"; exit 0;;
-  fc[0-9]*)
-    ver=`echo $1 | sed 's/fc//'`; echo "$ver:fc:fedora-$ver"; exit 0;;
-  el[0-9]*)
-    ver=`echo $1 | sed 's/el//'`; echo "$ver:el:rocky+epel-$ver"; exit 0;;
-  *)
+  *) echo "0:unk";exit 1;;
 esac
-echo 0:unk;
+exit 0
