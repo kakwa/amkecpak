60af05fcb4639cd87f3f038cf342c4f97b6ceda4
kakwa <carpentier.pf@gmail.com>
2025-02-18 22:55:36 +0100

    clean-up code

--- common/buildenv/skip_flag.sh
+++ common/buildenv/skip_flag.sh
@@ -2,14 +2,12 @@
 
 help(){
   cat <<EOF
-usage: `basename $0` -i <IGNORE_STRING> -d <DISTRIBUTION> -v <VERSION>
-
+usage: $(basename "$0") -i <IGNORE_STRING> -d <DISTRIBUTION> -v <VERSION>
 Check if current dist is to be ignored for build.
-
 Will print 'true' to stdout if the dist/version is to be ignored.
 
 example:
-  > `basename $0` -i '=:el:6 <:deb:8' -d deb -v 7
+  > $(basename "$0") -i '=:el:6 <:deb:8' -d deb -v 7
   true
 
 arguments:
@@ -18,9 +16,7 @@ arguments:
   -v <VERSION>:       the specific version to check
 
 ignore string format:
-
 The ignore space is a space separated list of rules.
-
 each rule have the format "<op>:<dist>:<version>", with:
   <op>:      the operation (must be  '>', '>=', '<', '<=' or '=')
   <dist>:    the distribution code name (examples: 'deb', 'el', 'fc')
@@ -33,34 +29,18 @@ exit_msg(){
   code=$1
   msg="$2"
   printf "%s\n" "$msg" >&2
-  exit $1
+  exit $code
 }
 
 while getopts ":hi:d:v:" opt; do
   case $opt in
 
-    h) 
-        help
-        ;;
-    i)
-        IGNORE_STRING="$OPTARG"
-        ;;
-    d)
-        DIST="$OPTARG"
-        ;;
-    v)
-        VERSION="$OPTARG"
-        ;;
-    \?)
-        echo "Invalid option: -$OPTARG" >&2
-        help
-        exit 1
-        ;;
-    :)
-        echo "Option -$OPTARG requires an argument." >&2
-        help
-        exit 1
-        ;;
+    h) help;;
+    i) IGNORE_STRING="$OPTARG";;
+    d) DIST="$OPTARG";;
+    v) VERSION="$OPTARG";;
+    \?) echo "Invalid option: -$OPTARG" >&2;help;exit 1;;
+    :)  echo "Option -$OPTARG requires an argument." >&2 ;help; exit 1;;
   esac
 done
 
@@ -68,45 +48,31 @@ done
 [ -z "$DIST" ]          && exit_msg 1 "missing -d <DISTRIBUTION>"
 [ -z "$VERSION" ]       && exit_msg 1 "missing -v <VERSION>"
 
-SKIPPED=0
-
 for rule in $IGNORE_STRING
 do
   rule_type=`echo $rule | cut -d ':' -f 1`
   rule_dist=`echo $rule | cut -d ':' -f 2`
   rule_version=`echo $rule | cut -d ':' -f 3`
-  if [ "$rule_dist" = "$DIST" ]
-  then
-    case $rule_type in
-      '>')  op=">";;
-      '>=') op=">=";;
-      '<') op="<";;
-      '<=') op="<=";;
-      '=') op="==";;
-    esac
-    if ! [ -z "$op" ]
-    then
-      # weird version comparaison
-      # limitation:
-      # * only work with up to 4 digits
-      # * only 4 digit max per section
-      # * no rc/beta/alpha possible
-      var=`awk "BEGIN {
-        # Split each arg in array
-        split(\"$VERSION\", FirstVersArr, \".\")
-        split(\"$rule_version\", SeconVersArr, \".\")
 
-        # Translate in decimal
-        FirstVersDec = FirstVersArr[1] * 10^12 + FirstVersArr[2] * 10^8 + FirstVersArr[3] * 1^4 + FirstVersArr[4]
-        SeconVersDec = SeconVersArr[1] * 10^12 + SeconVersArr[2] * 10^8 + SeconVersArr[3] * 1^4 + SeconVersArr[4]
+  [ "$rule_dist" = "$DIST" ] || continue
 
-        print (FirstVersDec $op SeconVersDec)}"`
-      if [ "$var" -eq 1 ];then SKIPPED=1;fi
-    fi
-  fi
+  # Perform version comparison
+  result=$(awk -v v1="$VERSION" -v v2="$rule_version" -v op="$rule_type" 'BEGIN {
+    split(v1, v1_arr, ".");
+    split(v2, v2_arr, ".");
+
+    # Convert version segments to numerical values
+    v1_dec = (v1_arr[1] * 10^12) + (v1_arr[2] * 10^8) + (v1_arr[3] * 10^4) + v1_arr[4];
+    v2_dec = (v2_arr[1] * 10^12) + (v2_arr[2] * 10^8) + (v2_arr[3] * 10^4) + v2_arr[4];
+
+    if (op == ">")       {print (v1_dec > v2_dec)  ? "true" : "false";}
+    else if (op == ">=") {print (v1_dec >= v2_dec) ? "true" : "false";}
+    else if (op == "<")  {print (v1_dec < v2_dec)  ? "true" : "false";}
+    else if (op == "<=") {print (v1_dec <= v2_dec) ? "true" : "false";}
+    else if (op == "=")  {print (v1_dec == v2_dec) ? "true" : "false";}
+    else {print "Invalid comparison operator" > "/dev/stderr"; exit 1};
+  }')
+  [ $? -ne 0 ] && exit_msg 1 "comparison error"
+
+  [ "$result" = "true" ] && exit_msg 0 "true"
 done
-
-if [ $SKIPPED -eq 1 ]
-then
-  printf "true\n"
-fi
