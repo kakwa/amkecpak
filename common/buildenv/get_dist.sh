1893afbd914adf36bf35aa535706a83434840474
kakwa <carpentier.pf@mail.com>
2025-02-21 14:00:23 +0100

    better handling of codenames

--- common/buildenv/get_dist.sh
+++ common/buildenv/get_dist.sh
@@ -25,47 +25,100 @@ EOF
 # Validate input arguments
 [ "$#" -ne 1 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]  && help
 
+# Generic Debian/Ubuntu mapping helper function
+get_codename() {
+    local version=$1
+    local type=$2
+    local codename=""
+
+    # Check local debootstrap scripts first
+    if [ -d "/usr/share/debootstrap/scripts" ]; then
+        for script in /usr/share/debootstrap/scripts/*; do
+            base_script=$(basename "$script")
+
+			# Skip versions already present
+			grep -q ":$base_script\"" $0 && continue
+
+			MIRROR=$(sed -n 's/ *default_mirror *//p' "$script" | head -n 1)
+			if [ -z "${MIRROR}" ]
+			then
+            case $type in
+                "deb") MIRROR="https://deb.debian.org/debian";;
+                "ubu") MIRROR="https://archive.ubuntu.com/ubuntu";;
+				*)     MIRROR="https://deb.debian.org/debian";;
+            esac
+			fi
+			data=$(curl -s "${MIRROR}/dists/${base_script}/Release" --range 0-512 | tr '[:upper:]' '[:lower:]')
+			if echo "$data" | grep -q "origin: ${type}" || echo "$data" | grep -q "label: ${type}" &&  echo "$data" | grep -q "version: ${version}"
+			then
+				codename=$base_script
+				break
+			fi
+        done
+    fi
+
+    # If no codename found, use development versions as a fallback
+    if [ -z "$codename" ]; then
+        case $type in
+            "deb") codename="unstable";;
+            "ubu") codename="devel";;
+			*)     codename="$3";;
+        esac
+    fi
+    echo "$codename"
+}
+
 case $1 in
   # Debian versions
-  wheezy|deb7) echo "7:deb:debian-7" ;;
-  jessie|deb8) echo "8:deb:debian-8" ;;
-  stretch|deb9) echo "9:deb:debian-9" ;;
-  buster|deb10) echo "10:deb:debian-10" ;;
-  bullseye|deb11) echo "11:deb:debian-11" ;;
-  bookworm|deb12) echo "12:deb:debian-12" ;;
-  trixie|deb13) echo "13:deb:debian-13" ;;
-  sid|deb14) echo "14:deb:debian-14" ;;
+  wheezy|deb7) echo "7:deb:debian-7:wheezy" ;;
+  jessie|deb8) echo "8:deb:debian-8:jessie" ;;
+  stretch|deb9) echo "9:deb:debian-9:stretch" ;;
+  buster|deb10) echo "10:deb:debian-10:buster" ;;
+  bullseye|deb11) echo "11:deb:debian-11:bullseye" ;;
+  bookworm|deb12) echo "12:deb:debian-12:bookworm" ;;
+  trixie|deb13) echo "13:deb:debian-13:trixie" ;;
+  sid|deb14) echo "14:deb:debian-14:sid" ;;
 
   # Ubuntu versions
-  precise|ubu12.04) echo "12.04:ubu:ubuntu-12.04" ;;
-  trusty|ubu14.04) echo "14.04:ubu:ubuntu-14.04" ;;
-  vivid|ubu15.04) echo "15.04:ubu:ubuntu-15.04" ;;
-  wily|ubu15.10) echo "15.10:ubu:ubuntu-15.10" ;;
-  xenial|ubu16.04) echo "16.04:ubu:ubuntu-16.04" ;;
-  yakkety|ubu16.10) echo "16.10:ubu:ubuntu-16.10" ;;
-  zesty|ubu17.04) echo "17.04:ubu:ubuntu-17.04" ;;
-  artful|ubu17.10) echo "17.10:ubu:ubuntu-17.10" ;;
-  bionic|ubu18.04) echo "18.04:ubu:ubuntu-18.04" ;;
-  cosmic|ubu18.10) echo "18.10:ubu:ubuntu-18.10" ;;
-  disco|ubu19.04) echo "19.04:ubu:ubuntu-19.04" ;;
-  eoan|ubu19.10) echo "19.10:ubu:ubuntu-19.10" ;;
-  focal|ubu20.04) echo "20.04:ubu:ubuntu-20.04" ;;
-  groovy|ubu20.10) echo "20.10:ubu:ubuntu-20.10" ;;
-  hirsute|ubu21.04) echo "21.04:ubu:ubuntu-21.04" ;;
-  impish|ubu21.10) echo "21.10:ubu:ubuntu-21.10" ;;
-  jammy|ubu22.04) echo "22.04:ubu:ubuntu-22.04" ;;
-  lunar|ubu23.04) echo "23.04:ubu:ubuntu-23.04" ;;
-  mantic|ubu23.10) echo "23.10:ubu:ubuntu-23.10" ;;
-  noble|ubu24.04) echo "24.04:ubu:ubuntu-24.04" ;;
-  oracular|ubu24.10) echo "24.10:ubu:ubuntu-24.10" ;;
-  plucky|ubu25.04) echo "25.04:ubu:ubuntu-25.04" ;;
+  precise|ubu12.04) echo "12.04:ubu:ubuntu-12.04:precise" ;;
+  trusty|ubu14.04) echo "14.04:ubu:ubuntu-14.04:trusty" ;;
+  vivid|ubu15.04) echo "15.04:ubu:ubuntu-15.04:vivid" ;;
+  wily|ubu15.10) echo "15.10:ubu:ubuntu-15.10:wily" ;;
+  xenial|ubu16.04) echo "16.04:ubu:ubuntu-16.04:xenial" ;;
+  yakkety|ubu16.10) echo "16.10:ubu:ubuntu-16.10:yakkety" ;;
+  zesty|ubu17.04) echo "17.04:ubu:ubuntu-17.04:zesty" ;;
+  artful|ubu17.10) echo "17.10:ubu:ubuntu-17.10:artful" ;;
+  bionic|ubu18.04) echo "18.04:ubu:ubuntu-18.04:bionic" ;;
+  cosmic|ubu18.10) echo "18.10:ubu:ubuntu-18.10:cosmic" ;;
+  disco|ubu19.04) echo "19.04:ubu:ubuntu-19.04:disco" ;;
+  eoan|ubu19.10) echo "19.10:ubu:ubuntu-19.10:eoan" ;;
+  focal|ubu20.04) echo "20.04:ubu:ubuntu-20.04:focal" ;;
+  groovy|ubu20.10) echo "20.10:ubu:ubuntu-20.10:groovy" ;;
+  hirsute|ubu21.04) echo "21.04:ubu:ubuntu-21.04:hirsute" ;;
+  impish|ubu21.10) echo "21.10:ubu:ubuntu-21.10:impish" ;;
+  jammy|ubu22.04) echo "22.04:ubu:ubuntu-22.04:jammy" ;;
+  lunar|ubu23.04) echo "23.04:ubu:ubuntu-23.04:lunar" ;;
+  mantic|ubu23.10) echo "23.10:ubu:ubuntu-23.10:mantic" ;;
+  noble|ubu24.04) echo "24.04:ubu:ubuntu-24.04:noble" ;;
+  oracular|ubu24.10) echo "24.10:ubu:ubuntu-24.10:oracular" ;;
+  plucky|ubu25.04) echo "25.04:ubu:ubuntu-25.04:plucky" ;;
 
   # Generic Mapping for fc/RHEL
-  fc[0-9]*) echo "${1#fc}:fc:fedora-${1#fc}" ;;
-  el[0-9]*) echo "${1#el}:el:rocky+epel-${1#el}" ;;
+  fc[0-9]*) echo "${1#fc}:fc:fedora-${1#fc}:${1}" ;;
+  el[0-9]*) echo "${1#el}:el:rocky+epel-${1#el}:${1}" ;;
+
   # Generic Debian/Ubuntu mapping
-  deb[0-9]*) echo "${1#deb}:deb:debian-${1#deb}" ;;
-  ubu[0-9]*\.[0-9]*) echo "${1#ubu}:ubu:ubuntu-${1#ubu}" ;;
+  deb[0-9]*)
+    version="${1#deb}"
+    codename=$(get_codename "$version" "deb" "$1")
+    echo "${version}:deb:debian-${version}:${codename}"
+    ;;
+
+  ubu[0-9]*\.[0-9]*)
+    version="${1#ubu}"
+    codename=$(get_codename "$version" "ubu" "$1")
+    echo "${version}:ubu:ubuntu-${version}:${codename}"
+    ;;
 
   *) echo "0:unk";exit 1;;
 esac
